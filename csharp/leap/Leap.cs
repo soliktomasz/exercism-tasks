fd23d463289875d5f45b8825952661c8d7cbefd2
Tomek <tomek.solik@gmail.com>
2019-06-11 22:58:42 +0200

    Added new tasks

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -1,4 +1,7 @@
 public static class Leap
 {
-    public static bool IsLeapYear(int year) => year % 4 == 0 ? year % 100 == 0 && year % 400 == 0 ? true : year % 100 != 0 : false;
+    public static bool IsLeapYear(int year)
+    {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
 }
---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        return Enumerable.Range(0, max).Where(i =>
+        {
+            foreach (var item in multiples)
+            {
+                if (item > 0 && i % item == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }).Sum();
+    }
+}
