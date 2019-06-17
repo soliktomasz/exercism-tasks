8234cc29dda8555f0779aa32c98617240c69a247
Tomek <tomek.solik@gmail.com>
2019-06-17 20:36:17 +0200

    Added Difference Of Squares task

---
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max) => (int)Math.Pow(Enumerable.Range(1, max).Sum(), 2);
+
+    public static int CalculateSumOfSquares(int max) => (int)Enumerable.Range(1, max).Sum(i => Math.Pow(i, 2));
+
+    public static int CalculateDifferenceOfSquares(int max) => Math.Abs(CalculateSumOfSquares(max) - CalculateSquareOfSum(max));
+}
